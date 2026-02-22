b92051e3651c8e803a8ceeb132b0137b698c1a60
DESKTOP-G1OBP59\ADMIN <pradeepdgenai@users.noreply.github.com>
2026-02-22 19:13:56 +0530

    Update template button text to 'Create new template'
    - Changed button text from 'Template' to 'Create new template' for clarity
    - All 5 E2E tests passing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -339,7 +339,7 @@ const Header: React.FC<HeaderProps> = ({
                 <rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect>
                 <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"></path>
               </svg>
-              Template
+              Create new template
             </button>
           )}
         </nav>
